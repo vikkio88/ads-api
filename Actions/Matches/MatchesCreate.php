ea209d5b4827438fd788c5432eb73d7bff8de241
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-12 16:39:35 +0000

    implemented missing action

--- Actions/Matches/MatchesCreate.php
+++ Actions/Matches/MatchesCreate.php
@@ -4,12 +4,15 @@
 namespace App\Actions\Matches;
 
 use App\Lib\Slime\RestAction\ApiAction;
+use App\Models\Match;
 
 class MatchesCreate extends ApiAction
 {
 
     protected function performAction()
     {
-        $this->payload = null;
+        $this->payload = Match::create(
+            $this->getJsonRequestBody()
+        );
     }
 }
