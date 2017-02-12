ae479b6161c5a9132a4711ed94ed1f59541a0323
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-12 12:03:30 +0000

    adding route for Team Actions

--- routes/teams/routes.php
+++ routes/teams/routes.php
@@ -1,11 +1,27 @@
 <?php
 
+use App\Actions\Teams\TeamsGetAll;
+use App\Actions\Teams\TeamsGetOne;
+use App\Actions\Teams\TeamsGetOneCoach;
+use App\Actions\Teams\TeamsGetOnePlayer;
+use App\Actions\Teams\TeamsGetOneWithRoster;
+
 $api->get('/teams', function ($request, $response, $args) {
-            return (
-                new SOMEACTION(
-                    $request,
-                    $response,
-                    $args
-                )
-             )->execute();
-             });
+    return (new TeamsGetAll($request, $response, $args))->execute();
+});
+
+$api->get('/teams/{id}', function ($request, $response, $args) {
+    return (new TeamsGetOne($request, $response, $args))->execute();
+});
+
+$api->get('/teams/{id}/players', function ($request, $response, $args) {
+    return (new TeamsGetOneWithRoster($request, $response, $args))->execute();
+});
+
+$api->get('/teams/{id}/coach', function ($request, $response, $args) {
+    return (new TeamsGetOneCoach($request, $response, $args))->execute();
+});
+
+$api->get('/teams/{id}/players/{playerId}', function ($request, $response, $args) {
+    return (new TeamsGetOnePlayer($request, $response, $args))->execute();
+});
