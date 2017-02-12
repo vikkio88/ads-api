b365a5b9e7bc26d369f68804cb09cae7ed76f5ef
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-12 11:03:56 +0000

    imported config

--- config/modules.php
+++ config/modules.php
@@ -1,4 +1,74 @@
 <?php
 
 return [
+    'modules' => [
+        "4-4-2" => [
+            'roles' => [1, 1, 2, 1, 1, 2, 1, 0, 2, 0],
+            'character' => 2
+        ],
+        "4-3-3" => [
+            'roles' => [1, 1, 2, 1, 0, 3, 0, 1, 1, 1],
+            'character' => 1
+        ],
+        "4-5-1" => [
+            'roles' => [1, 1, 2, 1, 1, 3, 1, 0, 1, 0],
+            'character' => 4
+        ],
+        "4-2-4" => [
+            'roles' => [1, 1, 2, 1, 0, 2, 0, 1, 2, 1],
+            'character' => 1
+        ],
+        "3-5-2" => [
+            'roles' => [1, 0, 3, 0, 1, 3, 1, 0, 2, 0],
+            'character' => 2
+        ],
+        "3-4-3" => [
+            'roles' => [1, 0, 3, 0, 1, 2, 1, 1, 1, 1],
+            'character' => 1
+        ],
+        "3-3-4" => [
+            'roles' => [1, 0, 3, 0, 0, 3, 0, 1, 2, 1],
+            'character' => 1
+        ],
+        "5-4-1" => [
+            'roles' => [1, 1, 3, 1, 1, 2, 1, 0, 1, 0],
+            'character' => 4
+        ],
+        "5-3-2" => [
+            'roles' => [1, 1, 3, 1, 0, 3, 0, 0, 2, 0],
+            'character' => 4
+        ]
+    ],
+    'roles' => [
+        'GK' => [
+            'description' => 'Goalkeeper'
+        ],
+        'LD' => [
+            'description' => 'Left Defender'
+        ],
+        'CD' => [
+            'description' => 'Central Defender'
+        ],
+        'RD' => [
+            'description' => 'Right Defender'
+        ],
+        'LM' => [
+            'description' => 'Left Midfielder'
+        ],
+        'CM' => [
+            'description' => 'Central Midfielder'
+        ],
+        'RM' => [
+            'description' => 'Right Midfielder'
+        ],
+        'LS' => [
+            'description' => 'Left Striker(Wing)'
+        ],
+        'CS' => [
+            'description' => 'Central Striker'
+        ],
+        'RS' => [
+            'description' => 'Right Striker(Wing)'
+        ],
+    ]
 ];
