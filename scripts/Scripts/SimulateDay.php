7e80002a95218985f5e923a7df2f015cb9703e67
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-05 23:00:26 +0000

    implementing day match simulation

--- scripts/Scripts/SimulateDay.php
+++ scripts/Scripts/SimulateDay.php
@@ -6,30 +6,50 @@ namespace App\Scripts;
 
 use App\Lib\AdsLib\MatchSimulator;
 use App\Models\LeagueRound;
+use App\Models\Match;
 use Carbon\Carbon;
 use Mashtru\Libs\Interfaces\Job;
 
 class SimulateDay implements Job
 {
+    private $now = null;
 
     public function fire(array $parameters = [])
     {
-        $now = Carbon::now();
-        /*
-        if ($now->hour >= 4 && $now->hour > 22) {
+        $this->now = Carbon::now();
+
+        if ($this->now->hour >= 4 && $this->now->hour > 22) {
             return false;
         }
-        */
-
-        $rounds = LeagueRound::where('date', '<', $now->toDateString())->get();
-        foreach ($rounds as $round) {
-            MatchSimulator::simulateRound($round->id);
-        }
-
+        $this->simulateMatches();
     }
 
     public function getName()
     {
         return "simulateDay";
+    }
+
+    private function simulateMatches()
+    {
+        $matches = Match::where('date', '<', $this->now->addDay()->toDateString())->get();
+        $roundIds = [];
+        foreach ($matches as $match) {
+            MatchSimulator::simulateCompleteResult($match->id);
+            $roundIds[$match->league_round_id] = true;
+        }
+
+        if (!empty($roundIds)) {
+            $simulatedRound = LeagueRound::whereDoesntHave(
+                'matches',
+                function ($query) {
+                    $query->where('simulated', false);
+                }
+            )->whereIn('id', array_keys($roundIds))->get();
+
+            foreach ($simulatedRound as $round) {
+                $round->simulated = true;
+                $round->save();
+            }
+        }
     }
 }
